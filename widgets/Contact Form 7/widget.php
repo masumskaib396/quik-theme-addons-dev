3ac25615034d1635581d63ac5e3f539459a20a6e
Jasimnwu <jasimnwu75@gmail.com>
2021-12-22 23:04:54 +0600

    crs

---
+++ widgets/Contact Form 7/widget.php
@@ -0,0 +1,45 @@
+<?php
+namespace Finest_Addons\Widgets;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+use \Elementor\Controls_Manager;
+use \Elementor\Group_Control_Border;
+use \Elementor\Group_Control_Image_Size;
+use Elementor\Core\Schemes\Typography;
+use Elementor\Group_Control_Typography;
+use \Elementor\Group_Control_Background;
+use Elementor\Group_Control_Box_Shadow;
+use Elementor\Group_Control_Css_Filter;
+use \Elementor\Icons_Manager;
+use \Elementor\Utils;
+use \Elementor\Widget_Base;
+
+class Finest_Contact_Form extends Widget_Base {
+
+	public function get_name() {
+		return 'finest-contact-form';
+	}
+
+	public function get_title() {
+		return esc_html__( 'Contact Form 7', 'finest-addons' );
+	}
+
+	public function get_icon() {
+		return 'eicon-mail';
+	}
+
+	public function get_categories() {
+		return [ 'finest-addons' ];
+	}
+
+	public function get_keywords() {
+		return [ 'Contact_Form_7', 'Form', 'Contact'];
+	}
+
+	protected function register_controls() {
+	}
+	protected function render() { } 
+
+}
+$widgets_manager->register_widget_type( new \Finest_Addons\Widgets\Finest_Contact_Form() );
